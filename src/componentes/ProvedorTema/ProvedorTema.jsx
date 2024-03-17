086292129fec22582f34db75dccfcbe01e55dc05
André Oliveira <andredev426@gmail.com>
2024-03-17 15:19:32 -0300

    temificando estilos globais

--- src/componentes/ProvedorTema/ProvedorTema.jsx
+++ src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -1,40 +1,46 @@
-import { ThemeProvider } from '@emotion/react' //  Importando componente "Theme Provaider"
+import { ThemeProvider } from '@emotion/react'
+
 
 const tema = {
-    cores: {
-        branco: '',
-        atencao: '',
-        focus: '',
+  cores: {
+    branco: '',
+    atencao: '',
+    focus: '',
 
-        primarias: {
-            a: '#5754ed',
-            b: '',
-            c: ''
-        },
-        secundarias: {
-            a: '#ebeaf9',
-            b: '',
-            c: '',
-        },
-        neutras: {
-            a: '',
-            b: '',
-            c: '',
-            d: ''
-        },
-        dark: {
-            a: '',
-            b: '',
-        }
+    primarias: {
+      a: '#5754ed',
+      b: '',
+      c: ''
     },
-    espacamentos: {
-        s : '16px',
-        l : '32px'
+    secundarias: {
+      a: '#ebeaf9',
+      b: '',
+      c: '',
+    }, 
+    neutras: {
+      a: '',
+      b: '',
+      c: '',
+      d: ''
+    },
+    dark: {
+      a: '',
+      b: '',
+      c: ''
     }
+  },
+  espacamentos: {
+    s: '16px',
+    l: '32px'
+  },
+  fontFamily: "'Montserrat', sans-serif"
 }
 
-export const ProvedorTema = ({ children }) => {
-    return <ThemeProvider theme={tema}>
-        { children }
+export const ProvedorTema = ({children}) => {
+  return(
+    <ThemeProvider theme={tema}>
+      {children}
     </ThemeProvider>
+  )
 }
+
